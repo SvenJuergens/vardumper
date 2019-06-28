206846a3bac8a5bcb7da83ca5d064d7ef5fb7e37
SvenJuergens <sj@nordsonne.de>
2019-06-28 19:43:17 +0200

    [TASK] Update to Symfony/VarDumper 4.3.2

--- Resources/Private/Libraries/vendor/symfony/var-dumper/Caster/DsPairStub.php
+++ Resources/Private/Libraries/vendor/symfony/var-dumper/Caster/DsPairStub.php
@@ -14,20 +14,15 @@ namespace Symfony\Component\VarDumper\Caster;
 use Symfony\Component\VarDumper\Cloner\Stub;
 
 /**
- * Represents a PHP constant and its value.
- *
  * @author Nicolas Grekas <p@tchwork.com>
  */
-class ConstStub extends Stub
+class DsPairStub extends Stub
 {
-    public function __construct(string $name, $value = null)
+    public function __construct($key, $value)
     {
-        $this->class = $name;
-        $this->value = 1 < \func_num_args() ? $value : $name;
-    }
-
-    public function __toString()
-    {
-        return (string) $this->value;
+        $this->value = [
+            Caster::PREFIX_VIRTUAL.'key' => $key,
+            Caster::PREFIX_VIRTUAL.'value' => $value,
+        ];
     }
 }
