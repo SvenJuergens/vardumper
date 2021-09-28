6f3dba5d4b2fb867be67a1927ee1b216899d1a78
SvenJuergens <SvenJuergens@users.noreply.github.com>
2021-09-28 17:06:17 +0200

    [TASK] Update Symfony VarDumper to 5.3.8

--- Resources/Private/Libraries/vendor/composer/autoload_static.php
+++ Resources/Private/Libraries/vendor/composer/autoload_static.php
@@ -7,8 +7,8 @@ namespace Composer\Autoload;
 class ComposerStaticInitae4150b2844f2afe1f458cff6b17e286
 {
     public static $files = array (
-        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
     );
 
